41a6a86134bf485a40212f57d39f493882f98e45
littlebassistjm <krishanhilario@gmail.com>
2017-04-07 23:49:03 +0800

    Added SelectSurveyActivity

---
+++ app/src/main/java/com/example/dominic/paperless/SurveyAdapter.java
@@ -0,0 +1,25 @@
+package com.example.dominic.paperless;
+
+/**
+ * Created by jmkhilario on 07/04/2017.
+ */
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SurveyAdapter extends RecyclerView.Adapter<RecyclerAdapter.SurveyHolder>{
+    @Override
+    public RecyclerAdapter.SurveyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerAdapter.SurveyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
