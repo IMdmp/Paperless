95501d62ab57cd74726051b3d306f749c30f099f
littlebassistjm <krishanhilario@gmail.com>
2017-04-08 04:52:13 +0800

    quantitative and qualitative recyclerviewssss

---
+++ app/src/main/java/com/example/dominic/paperless/ViewStatisticsActivity.java
@@ -0,0 +1,13 @@
+package com.example.dominic.paperless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_statistics);
+    }
+}
