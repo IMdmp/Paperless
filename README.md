aa0289c328a21b7f9cf9557db24e1b019d54ab74
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2019-01-09 23:34:50 +0800

    initial files.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Paperless
+
+dd/mm/yyyy
+09/01/2019 - >  start from scratch; old code just used for reference.
---
+++ app/src/main/java/com/dmp/project/paperless/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dmp.project.paperless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
