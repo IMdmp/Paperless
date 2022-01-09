fa5944eca6aa8b06ce1b5f9cc478c5ec1acbcd2f
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-01-09 23:02:04 +0800

    added flipper

--- app/src/main/java/com/imdmp/paperless/PaperlessApplication.kt
+++ app/src/main/java/com/imdmp/paperless/PaperlessApplication.kt
@@ -2,13 +2,28 @@ package com.imdmp.paperless
 
 import android.app.Application
 import timber.log.Timber
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.soloader.SoLoader
 
 class PaperlessApplication : Application(){
-
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
+        SoLoader.init(this, false);
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin( DatabasesFlipperPlugin(this));
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.start()
+        }
     }
 }
